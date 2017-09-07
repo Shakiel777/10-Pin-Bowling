029d59bb4e5f0137c71c606731697677aff8507d
DavidRood <shakiel@roadrunner.com>
2017-09-06 20:58:22 -0400

    set up launch bal with swipe, ui panel setup

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,39 +3,23 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 
-    private AudioSource audioSource;
-    private Rigidbody rigidBody;
-
-    private bool hasStarted = false;
     public Vector3 launchVelocity;
-
+    
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
 
 	// Use this for initialization
 	void Start ()
     {
         rigidBody = GetComponent<Rigidbody>();
-        audioSource = GetComponent<AudioSource>();
+        rigidBody.useGravity = false;
 	}
 
-    // Update is called once per frame
-    void Update()
+    public void Launch(Vector3 velocity)
     {
-        Launch();
-
-    }
-
-    private void Launch()
-    {
-        if (!hasStarted)
-        {
-            // waiting for mouse click to launch
-            if (Input.GetMouseButtonDown(0))
-            {
-                print(" Mouse Clicked, launch ball");
-                hasStarted = true;
-                this.GetComponent<Rigidbody>().velocity = launchVelocity;
-                audioSource.Play();
-            }
-        }
+        rigidBody.useGravity = true;
+        GetComponent<Rigidbody>().velocity = velocity;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
     }
 }
