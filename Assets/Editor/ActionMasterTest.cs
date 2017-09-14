b8449f768d952a5cfbe8aacf118c2543ca50d99a
DavidRood <shakiel@roadrunner.com>
2017-09-13 21:30:57 -0400

    continued work on actionmaster

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -7,8 +7,17 @@ using UnityEngine;
 [TestFixture]
 public class ActionMasterTest
 {
+    private ActionMaster actionMaster;
     private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+    private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+    private ActionMaster.Action reset = ActionMaster.Action.Reset;
     
+    [SetUp]
+    public void Setup()
+    {
+        // ActionMaster actionMaster = new ActionMaster();
+        actionMaster = new ActionMaster();
+    }
 
     [Test]
     public void T00PassingTest()
@@ -18,8 +27,22 @@ public class ActionMasterTest
     [Test]
     public void T01OneStrikeReturnsEndTurn()
     {
-        ActionMaster actionMaster = new ActionMaster();
+
         Assert.AreEqual(endTurn, actionMaster.Bowl(10));
     }
+    [Test]
+    public void T02Bowl8ReturnsTidy()
+    {
+
+        Assert.AreEqual(tidy, actionMaster.Bowl(8));
+    }
+    [Test]
+    public void T03Bowl28ReturnsEndTurn()
+    {
+        actionMaster.Bowl(8);
+        Assert.AreEqual(endTurn, actionMaster.Bowl(2));
+    }
+
+
 }
 
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -5,22 +5,33 @@ public class ActionMaster {
 
     public enum Action { Tidy, Reset, EndTurn, EndGame};
 
+    // private int[] bowls = new int[21];  // reminder - arrays start at zero not 1
+    private int bowl = 1;
+
     public Action Bowl (int pins)
     {
         if(pins < 0 || pins > 10){throw new UnityException("Invalid pin count < 0 or > 10!");}
 
-        if (pins == 10)
+        if (pins == 10) // this is a strike
         {
+            bowl += 2; // move to next frame
+            return Action.EndTurn;
+        }
+
+        if (bowl % 2 != 0) // we are mid-frame (or last frame)
+        {
+            bowl += 1;  // bump frame to end of frame
+            return Action.Tidy;
+        }
+        else if (bowl % 2 == 0) // we are at end of frame 
+        {
+            bowl += 1;
             return Action.EndTurn;
         }
 
 
-
+        // refactor code from ugly and working to pretty and working!
 
         throw new UnityException("Not sure what action to return!");
-
-        // return Action.Tidy;
     }
-
-
 }
